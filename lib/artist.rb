950227cf8a02ab633b212dcc1f7de2b962250d78
Collins <mutaic84@gmail.com>
2022-06-14 10:44:42 +0300

    refactor findable module

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,6 +5,7 @@ class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable
+  extend Findable
 
   attr_accessor :name
   attr_reader :songs
@@ -14,10 +15,6 @@ class Artist
   def initialize
     super
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
--- lib/song.rb
+++ lib/song.rb
@@ -5,15 +5,12 @@ class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable
+  extend Findable
 
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
 
   def self.all
     @@songs
